f369810f1826783f5aafd8395bbd6c291cf02b73
Elif Özcan <ozcann159@gmail.com>
2024-10-14 17:26:45 +0300

    Ders 3

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If
+            //Console.Write("Lütfen Şifreyi Giriniz:");
+            //string password;
+            //password= Console.ReadLine();
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre doğru");
+            //}
+            //else {
+            //    Console.WriteLine("Şifre yanlış");
+            //}
+
+            string capital, country;
+            Console.WriteLine("Başkenti giriniz: ");
+            capital = Console.ReadLine();
+
+            Console.Write("Ülkeyi giriniz: ");
+            country = Console.ReadLine();
+
+            if (capital == "ankara" & country == "türkiye")
+            {
+                Console.WriteLine("Veriler doğrulandı");
+            }
+            else
+            {
+                Console.WriteLine("Veriler hatalı");
+            }
+
+            #endregion
+            #region
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
